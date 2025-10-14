ffb2c186fb4bc31a09d9d815498adca597b79c50
Giovane Santos <giovanesantos1999@gmail.com>
2025-10-13 21:47:45 -0300

    bugfix(test): change value in product mock repository

--- src/infra/repository/mock/Product.repository.ts
+++ src/infra/repository/mock/Product.repository.ts
@@ -1,7 +1,7 @@
 import { Product } from "~@Domain/entity/Product.entity";
 import { IProductRepository } from "~@Domain/repository/IProduct.repository";
 
-const newProduct = new Product("1", "Product 1", 100);
+const newProduct = new Product("1", "Product 1", 10);
 
 export const ProductRepositoryMock = function (): jest.Mocked<IProductRepository> {
 	return {
